ca42b1473e94d2374811ba2a660dfcd07ec1bcfb
kamalprasadpandey <kamalprasadpandey@gmail.com>
2022-11-28 22:35:35 +0530

    28 Nov

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   //Slider settings
-  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1} ;
+  slideConfig = { autoplay: true, "slidesToShow": 1, "slidesToScroll": 1} ;
   constructor() { }
 
   ngOnInit(): void {
